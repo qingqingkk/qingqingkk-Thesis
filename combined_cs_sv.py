7935528b13d4b3b1098d5305e2de95fa350842d3
qingqingkk <liu395778076@gmail.com>
2025-01-12 13:58:30 +0100

    fix bug

--- combined_cs_sv.py
+++ combined_cs_sv.py
@@ -110,15 +110,16 @@ def combine_cs_sv(data_path, output_dir):
     # Compress folders to zip files
     if args.save_compress:
         def _zip_folder(audio_path, csv_file, zip_path):
-            with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-                for root, _, files in tqdm(os.walk(audio_path)):
-                    for file in files:
-                        zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), audio_path))
-            if os.path.exists(csv_file):
-                zipf.write(csv_file, os.path.basename(csv_file))
-            else:
-                raise FileNotFoundError(f"Cannot find {csv_file}")
-            print(f'Compressed file are save in {zip_file_path}')
+                print('Start compress data...')
+                with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                    for root, _, files in os.walk(audio_path):
+                        for file in tqdm(files):
+                            zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), audio_path))
+                        if os.path.exists(csv_file):
+                            zipf.write(csv_file, os.path.basename(csv_file))
+                        else:
+                            raise FileNotFoundError(f"Cannot find {csv_file}")
+                print(f'Compressed file are save in {zip_file_path}')
         # Call function to compress folder
         zip_file_path = os.path.join(output_folder, "CS_SV_concat.zip") 
         _zip_folder(output_folder, updated_csv_file, zip_file_path)
